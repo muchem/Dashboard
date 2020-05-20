f5c5f09e520b7abeb944545b71c21a8eca8f60e5
muchem <melissauchem@gmail.com>
2020-05-20 18:50:16 -0500

    render crypto news

--- src/app/Components/crypto/crypto.component.ts
+++ src/app/Components/crypto/crypto.component.ts
@@ -1,15 +1,92 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from 'src/app/Services/data.service';
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-crypto',
   templateUrl: './crypto.component.html',
   styleUrls: ['./crypto.component.scss']
 })
 export class CryptoComponent implements OnInit {
+  BTCDaily;
+  btcDailyChart = [];
+  btcDailyDates = [];
+  BtcDailyValues = [];
 
-  constructor() { }
+  cryptoArticles;
+  constructor(private Service:DataService) { }
 
   ngOnInit() {
+    this.Service.getBitcoinCurrency().subscribe(ob =>{
+      this.BTCDaily =  Object.entries(ob['Time Series (Digital Currency Daily)']).splice(0,5);
+      console.log(this.BTCDaily);
+      for(let i = 0; i< this.BTCDaily.length; i++){
+        this.btcDailyDates.push(this.BTCDaily[i][0]);
+        this.BtcDailyValues.push(this.BTCDaily[i][1]['1a. open (USD)']);
+      }
+      this.btcDailyChart = new Chart('BTChart', {
+        type: 'line',
+        data: {
+        labels:this.btcDailyDates,
+          datasets: [
+            { 
+              data: this.BtcDailyValues,
+              label: "Open",
+              pointRadius: 1,
+              lineTension: 0,
+              borderWidth: 2,
+              borderColor: "#3e95cd",
+              backgroundColor:'#3e95cd',
+              fill:false
+            }
+            
+          ]
+        },
+        options: {
+          title: {
+						display: true,
+						text: 'Bitcoin'
+					},
+          scales: {
+            xAxes: [{
+              display: true,
+              offset: true,
+              ticks: {
+                major: {
+                  enabled: true,
+                  fontStyle: 'bold'
+                },
+                source: 'data',
+          
+                sampleSize: 5
+              },
+              
+              scaleLabel: {
+                display: true,
+                labelString: 'Date'
+              }
+            }]
+          }
+        },
+        tooltips: {
+          intersect: false,
+          mode: 'index',
+          callbacks: {
+            label: function(tooltipItem, myData) {
+              var label = myData.datasets[tooltipItem.datasetIndex].label || '';
+              if (label) {
+                label += ': ';
+              }
+              label += parseFloat(tooltipItem.value).toFixed(2);
+              return label;
+            }
+          }
+        }
+    })
+  });
+  this.Service.getCryptoNews().subscribe(articles =>{
+    this.cryptoArticles = articles.splice(1,18);
+ 
+  })
   }
 
 }
--- src/app/Services/data.service.ts
+++ src/app/Services/data.service.ts
@@ -19,34 +19,40 @@ export class DataService {
     return this.http.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${this.selectedSynbol.param.updates[0].value}&interval=5min&apikey=UHKM95R37PUMWX1E`);
   }
   getDaily(companySymbol:string):Observable<any>{
-    let symbol= new HttpParams().set('query',companySymbol);
+    let symbol= new HttpParams().set('symbol',companySymbol);
     this.selectedSynbol = { param: symbol };
     return this.http.get<any>(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${this.selectedSynbol.param.updates[0].value}&apikey=UHKM95R37PUMWX1E`);
   }
   getRecommendationTrend(companySymbol:string):Observable<any>{
-    let symbol= new HttpParams().set('query',companySymbol);
+    let symbol= new HttpParams().set('symbol',companySymbol);
     this.selectedSynbol = { param: symbol }; 
     return this.http.get<any>(`https://finnhub.io/api/v1/stock/recommendation?symbol=${this.selectedSynbol.param.updates[0].value}&token=bq3rdo7rh5rb0pdpg08g`);
   }
   getPriceTargets(companySymbol:string):Observable<any>{
-    let symbol= new HttpParams().set('query',companySymbol);
+    let symbol= new HttpParams().set('symbol',companySymbol);
     this.selectedSynbol = { param: symbol }; 
     return this.http.get<any>(`https://finnhub.io/api/v1/stock/price-target?symbol=${this.selectedSynbol.param.updates[0].value}&token=bq3rdo7rh5rb0pdpg08g`);
   }
   getRevenue(companySymbol:string):Observable<any>{
-    let symbol= new HttpParams().set('query',companySymbol);
+    let symbol= new HttpParams().set('symbol',companySymbol);
     this.selectedSynbol = { param: symbol }; 
     return this.http.get<any>(`https://finnhub.io/api/v1/calendar/earnings?from=2018-01-01&symbol=${this.selectedSynbol.param.updates[0].value}&token=bq3rdo7rh5rb0pdpg08g`);
   }
   getEarningsCalender(companySymbol:string):Observable<any>{
-    let symbol= new HttpParams().set('query',companySymbol);
+    let symbol= new HttpParams().set('symbol',companySymbol);
     this.selectedSynbol = { param: symbol }; 
     return this.http.get<any>(`https://finnhub.io/api/v1/calendar/earnings?symbol=${this.selectedSynbol.param.updates[0].value}&token=bq3rdo7rh5rb0pdpg08g`);
   }
   getCompanyNews(companySymbol:string):Observable<any>{
-    let symbol= new HttpParams().set('query',companySymbol);
+    let symbol= new HttpParams().set('symbol',companySymbol);
     this.selectedSynbol = { param: symbol }; 
     return this.http.get<any>(`https://finnhub.io/api/v1/company-news?symbol=${this.selectedSynbol.param.updates[0].value}&from=2020-01-01&to=2020-05-01&token=bq3rdo7rh5rb0pdpg08g`);
+  }
+  getBitcoinCurrency():Observable<any>{
+    return this.http.get<any>(`https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=USD&apikey=UHKM95R37PUMWX1E`);
+  }
+  getCryptoNews():Observable<any>{
+    return this.http.get<any>(`https://finnhub.io/api/v1/news?category=crypto&token=bq3rdo7rh5rb0pdpg08g`);
   }
   getGeneralNews():Observable<any>{
     return this.http.get<any>(`https://finnhub.io/api/v1/news?category=general&token=bq3rdo7rh5rb0pdpg08g`);

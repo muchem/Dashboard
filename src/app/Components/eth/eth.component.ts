5833be5fa87ef92e602716ad00d676a255d9f3f5
muchem <melissauchem@gmail.com>
2020-06-01 14:14:17 -0500

    render crypto graphs

--- src/app/Components/eth/eth.component.ts
+++ src/app/Components/eth/eth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from 'src/app/Services/data.service';
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-eth',
   templateUrl: './eth.component.html',
@@ -7,9 +8,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EthComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private Service :DataService) { }
+  cryptoDaily;
+  dailyDates = [];
+  dailyValues = [];
+  dailyChart = []
   ngOnInit() {
+    this.Service.getCryptoCurrency("ETH").subscribe(obj =>{
+      this.cryptoDaily = Object.entries(obj['Time Series (Digital Currency Daily)']).splice(0,5);
+      for(let i = 0; i< this.cryptoDaily.length; i++){
+        this.dailyDates.push(this.cryptoDaily[i][0]);
+        this.dailyValues.push(this.cryptoDaily[i][1]['1a. open (USD)']);
+      }
+      this.dailyChart = new Chart('DailyChart', {
+        type: 'line',
+        data: {
+        labels:this.dailyDates.reverse(),
+          datasets: [
+            { 
+              data: this.dailyValues.reverse(),
+              label: "Open",
+              pointRadius: 1,
+              lineTension: 0,
+              borderWidth: 2,
+              borderColor: "#3e95cd",
+              backgroundColor:'#3e95cd',
+              fill:false
+            }
+            
+          ]
+        },
+        options: {
+          title: {
+						display: true,
+						text: 'Ethereum'
+					},
+          scales: {
+            xAxes: [{
+              display: true,
+              offset: true,
+              ticks: {
+                major: {
+                  enabled: true,
+                  fontStyle: 'bold'
+                },
+                source: 'data',
+          
+                sampleSize: 5
+              },
+              
+              scaleLabel: {
+                display: true,
+                labelString: 'Date'
+              }
+            }]
+          }
+        },
+        tooltips: {
+          intersect: false,
+          mode: 'index',
+          callbacks: {
+            label: function(tooltipItem, myData) {
+              var label = myData.datasets[tooltipItem.datasetIndex].label || '';
+              if (label) {
+                label += ': ';
+              }
+              label += parseFloat(tooltipItem.value).toFixed(2);
+              return label;
+            }
+          }
+        }
+    })
+    })
   }
 
 }

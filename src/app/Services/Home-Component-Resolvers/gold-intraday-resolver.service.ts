2b329329eba6299d6096c00160a3379cc90e2056
muchem <melissauchem@gmail.com>
2020-06-17 11:45:04 -0500

    add babel dependency

--- src/app/Services/Home-Component-Resolvers/gold-intraday-resolver.service.ts
+++ src/app/Services/Home-Component-Resolvers/gold-intraday-resolver.service.ts
@@ -1,14 +1,37 @@
 import { Injectable } from '@angular/core';
 import { DataService } from 'src/app/Services/data.service';
 import { Observable } from 'rxjs/Observable';
-import { Resolve } from '@angular/router';
+import { Resolve, Router } from '@angular/router';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
 @Injectable({
   providedIn: 'root'
 })
 export class GoldIntradayResolverService implements Resolve<any>{
 
-  constructor(private Service:DataService) { }
+  constructor(private Service:DataService, private router:Router) { }
   resolve():Observable<any[]>{
-    return this.Service.getIntradayData('GOLD');
+    return this.Service.getIntradayData('GOLD').map(response => {
+      if(response){
+        return response;
+        console.log("Responce Returned");
+      }
+      console.log("error");
+      this.router.navigate(['/','Barrick Gold Corp','/','ABX.TO']).then(nav =>{
+        console.log(nav);
+        return Observable.of(null);
+      }, err => {
+        console.log(err);
+      })
+    }).catch(error => {
+      console.log(error)
+      this.router.navigate(['/','Barrick Gold Corp','/','ABX.TO']).then(nav =>{
+        console.log(nav);
+      }, err =>{
+        console.log(err);
+      });
+      return Observable.of(null);
+    })
   }
 }
